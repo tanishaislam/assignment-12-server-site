4911bc8c688f2a67bf2d22b65a0f604265c5b0fc
tanishaislam <dev.eva.2006@gmail.com>
2023-11-27 19:14:10 +0600

    user update and remove api

--- index.js
+++ index.js
@@ -33,6 +33,32 @@ async function run() {
     const roomCollection = client.db("assignment12").collection("rooms");
     const cartsCollection = client.db("assignment12").collection("carts");
 
+    //jwt related api
+    app.post('/jwt', async (req, res) =>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '4h'})
+        res.send({ token })
+    })
+
+    //middelware
+    const verifyToken = (req, res, next) =>{
+        console.log('inside verify token', req.headers.authorization)
+        if(!req.headers.authorization){
+            res.status(401).send({message: 'forbidded access'})
+        }
+        const token = req.headers.authorization.split(' ')[1]
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+            if(err){
+                res.status(401).send({message: 'forbidded access'})
+            }
+            req.decoded = decoded;
+            next()
+        })
+        
+    }
+
+
+
     //user related apis
     app.post('/users', async (req, res) =>{
         const user = req.body;
@@ -42,6 +68,10 @@ async function run() {
             return res.send({messsage: 'user already exist', insertedId: null})
         }
         const result = await usersCollection.insertOne(user)
+        res.send(result)
+    })
+    app.get('/users', async (req, res) =>{
+        const result = await usersCollection.find().toArray()
         res.send(result)
     })
 
@@ -57,7 +87,7 @@ async function run() {
     })
 
     //agriment cart 
-    app.get('/carts', async (req, res) =>{
+    app.get('/carts', verifyToken, async (req, res) =>{
         const result = await cartsCollection.find().toArray()
         res.send(result)
     })
@@ -75,6 +105,17 @@ async function run() {
         res.send(result)
     })
 
+    // app.patch('/carts/member/:id', async (req, res) =>{
+    //     const id = req.params.id;
+    //     const filter = {_id: new ObjectId(id)}
+    //     const updateDoc = {
+    //         $set: {
+    //             role: 'member'
+    //         }
+    //     }
+    //     const result = await cartsCollection.updateOne(filter, updateDoc)
+    //     res.send(result)
+    // })
     app.patch('/carts/member/:id', async (req, res) =>{
         const id = req.params.id;
         const filter = {_id: new ObjectId(id)}
@@ -98,6 +139,21 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/carts/member/:email',verifyToken, async(req, res)=>{
+        const email = req.params.email;
+        if(email !== req.decoded.email){
+          return res.status(403).send({message: 'unauthrozied access'})
+        }
+        const query = {email: email};
+        const user = await cartsCollection.findOne(query)
+  
+        let member = false;
+        if(user){
+          member = user?.role === 'member'
+        }
+        res.send({member})
+  
+      })
 
 
 
